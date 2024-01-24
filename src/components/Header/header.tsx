82752e9c8f3fc05db4c2df7a8a4ed8654fda1d1b
Gabriel Carvalho <gabriel225gc@gmail.com>
2024-01-24 16:44:29 -0300

    feat: create new section higiene

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -26,7 +26,7 @@ export function Header() {
       </div>
 
       <ul className={styles.list}>
-        {['Bebidas', 'Frios', 'Limpeza', 'Padaria', 'Feira', 'Doces'].map((category, index) => (
+        {['Bebidas', 'Frios', 'Higiene', 'Padaria', 'Feira', 'Doces'].map((category, index) => (
           <li key={index}>
             <a
               href="#"
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -6,12 +6,14 @@ export function Main() {
 
   const bebidas = produtos.filter((produto) => produto.category === 'bebidas');
   const frios = produtos.filter((produto) => produto.category === 'frios');
+  const higiene = produtos.filter((produto) => produto.category === 'higiene');
 
   return (
     <main>
       <SearchFilter data={produtos}/>
       <ProductList products={bebidas} category="Bebidas" />
       <ProductList products={frios} category="Frios" />
+      <ProductList products={higiene} category="Higiene" />
     </main>
   );
 }
