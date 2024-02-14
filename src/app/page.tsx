221e85e60b5dc83daa729c47b1b92c27009f6a80
Gabriel Carvalho <gabriel225gc@gmail.com>
2024-02-14 16:32:41 -0300

    feat: add animations to cart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import { SearchFilter } from '@/components/SearchFilter/SearchFilter';
 import './globals.css';
 import { Header } from '@/components/Header/header'
 import { Main } from '@/components/Main/Main';
-import { Cart } from '@/components/Cart/Cart';
-
 
 export default function Home() {
   return (
--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -3,7 +3,7 @@
 import { ShoppingBasket } from '../Icons/ShoppingBasket';
 import styles from './Header.module.scss';
 import { ShoppingCart } from '../Icons/ShoppingCart';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Cart } from '../Cart/Cart';
 import { MenuLinks } from './MenuLinks';
 import { useTotalValue } from '@/contexts/CartContext';
@@ -11,11 +11,24 @@ import { formatPrice } from '@/hooks/formatPrice';
 
 export function Header() {
   const [showCart, setShowCart] = useState(false);
+  const [prevCartValue, setPrevCartValue] = useState(0);
   const cartValue = useTotalValue();
 
   const toggleCart = () => {
     setShowCart(!showCart);
   }
+
+  useEffect(() => {
+    if (cartValue !== prevCartValue) {
+      document.getElementById('cartButton')?.classList.add(styles.swing);
+
+      setTimeout(() => {
+        document.getElementById('cartButton')?.classList.remove(styles.swing);
+      }, 500);
+
+      setPrevCartValue(cartValue);
+    }
+  }, [cartValue, prevCartValue]);
 
   return (
     <header className={styles.header}>
@@ -24,7 +37,11 @@ export function Header() {
           <ShoppingBasket/>
           <h1>Mercado prático</h1>
         </a>
-        <button className={styles.cartButton} onClick={toggleCart}>
+        <button 
+          className={styles.cartButton} 
+          onClick={toggleCart} 
+          id="cartButton"
+        >
           <ShoppingCart/>
           <span>{formatPrice(cartValue)}</span>
         </button>
